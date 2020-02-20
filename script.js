a3a7648dd90572637014edbfe94d3c855faee9ac
MariaDheilany <maria.limadh@hotmail.com>
2020-02-19 22:36:34 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function()) {
+
+    $('.my-class').slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 2,
+        dats: false,
+        speed: 400,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        easing: 'linear',
+        arrows: true,
+        fade: false,
+        pauseOnHover: true,
+        swipe: true,
+
+    })
+};
